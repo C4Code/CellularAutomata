5578198cdd1544f286c46351adb6165c5a43f77d
-- C4 -- <info@c4ios.com>
2014-12-10 18:21:31 -0800

    Adding Async Version of CA
    Easier than hacking the working, but super slow version

---
+++ CellularAutomataAsync/CellularAutomataAsync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CellularAutomataAsync
+//
+//  Created by travis on 2014-12-10.
+//  Copyright (c) 2014 C4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
